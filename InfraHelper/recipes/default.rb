43dc7b630b50251b2772e985ca488f9d4679096c
Chris Munns <chrismunns@gmail.com>
2014-10-14 13:39:22 -0400

    pretty clear this isn't running in the right dir

--- InfraHelper/recipes/default.rb
+++ InfraHelper/recipes/default.rb
@@ -91,7 +91,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:current_path]}"
     code <<-EOH
-      command "/usr/local/bin/ruby #{deploy[:current_path]}/IHQueueWatcher_control.rb restart"
+      command "cd /srv/www/infrahelper/current; /usr/local/bin/ruby /srv/www/infrahelper/current/IHQueueWatcher_control.rb restart"
     EOH
   end
 
