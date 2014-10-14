06db74fdb7c24a3b0167719ea208cdd9abae8647
Chris Munns <chrismunns@gmail.com>
2014-10-14 13:23:38 -0400

    wrong ruby perhaps?

--- InfraHelper/recipes/default.rb
+++ InfraHelper/recipes/default.rb
@@ -91,7 +91,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:current_path]}"
     code <<-EOH
-      command "/usr/bin/ruby #{deploy[:current_path]}/IHQueueWatcher_control.rb restart"
+      command "/usr/local/bin/ruby #{deploy[:current_path]}/IHQueueWatcher_control.rb restart"
     EOH
   end
 
